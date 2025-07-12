2dd16aa3154bd29a88797b2d50a250e44be00e41
wahabbhai9571 <dilkhushverma8824@gmail.com>
2025-07-12 20:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,14 +17,14 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "29490954")
-API_HASH = os.getenv("API_HASH", "dbd8f5af56b0f6e16327c20a84eece99")
+API_ID = os.getenv("API_ID", "25405777")
+API_HASH = os.getenv("API_HASH", "6bff46327fffc7e03fa30a1ed19c5ce0")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7901828757:AAGN8XUQmEq-ictQYHnQRPU8_o0RZ_IOuB8")
-MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://wb7257490:Z6MX9jDVsKO7W2FI@cluster0.wmf3wk5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7660860610").split())) # list seperated via space
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://fibegi:8oV4fjNNVasSfcoY@cluster0.jp8thup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "8007695130").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002702928328")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002648848764")) # optional with -100
 FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002843859964")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
@@ -33,5 +33,5 @@ INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/DILKHUSH_BHAI_88")
 
